05ab30f7db36e3a740b602531aaa7e638776a58a
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2024-09-29 20:11:16 +0530

    devansshh

--- index.js
+++ index.js
@@ -38,14 +38,15 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get(
-//   "/",
-//   wrapAsync((req, res) => {
-//     // res.send("hii , i am root");
-    
+app.get(
+  "/",
+  wrapAsync(async(req, res) => {
+    // res.send("hii , i am root");
+    const allListings =  await Listing.find({});
 
-//     res.render("listings/index.ejs", { allListings });  })
-// );
+
+    res.render("listings/index.ejs", { allListings });  })
+);
 
 // this code is used to aUthenticate the user so that the user login agaian is the website they do not need to login again and again
 
@@ -81,13 +82,13 @@ const validateListing = (req, res, next) => {
 // });
 
 //Index Route
-app.get(
-  "/",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-  })
-);
+// app.get(
+//   "/listings",
+//   wrapAsync(async (req, res) => {
+//     const allListings = await Listing.find({});
+//     res.render("listings/index.ejs", { allListings });
+//   })
+// );
 
 // buy route
 app.get(
